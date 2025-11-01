98bc686053f8c3ea99c01b71e39f3861eea3345a
Raditya Rafa Pratama <rafapratama@student.ub.ac.id>
2025-11-02 01:29:16 +0700

    Update dosen.java

--- dosen.java
+++ dosen.java
@@ -11,24 +11,23 @@ public class dosen {
         this.mahasiswaBimbingan = new ArrayList<>();
     }
 
-    //method US 1
     public void tambahMahasiswaBimbingan(Mahasiswa mhs){
         this.mahasiswaBimbingan.add(mhs);
         System.out.println("INFO: Mahasiswa " + mhs.getNama() + " berhasil ditambahkan ke bimbingan dosen " + this.nama);
     }
 
-    //method US 2
     public void lihatDaftarBimbingan(){
         System.out.println("Daftar Mahasiswa Bimbingan Dosen " + this.nama + " ---");
         for(Mahasiswa mhs : this.mahasiswaBimbingan){
             System.out.println("- " + mhs.getNama() + " (" + mhs.getNim() + ")");
         }
     }
-        //getter
+    
     public String getNama() {
         return this.nama;
 }
     public String getNim() {
         return this.nidn;
     }
+
 }
